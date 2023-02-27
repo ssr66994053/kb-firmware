5ab920035bfd125481208f2c28b7426e64bb0872
sushiran <su.shiran@163.com>
2023-02-27 14:01:39 +0800

    key map

---
+++ split58/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBD, 0x9F, 0xD6, 0xB5, 0xAD, 0x2A, 0xE7, 0x67}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
---
+++ split58_f/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x57, 0xF4, 0x10, 0xE3, 0xB3, 0xFC, 0xB0, 0xA2}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
